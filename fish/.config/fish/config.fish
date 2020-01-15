bb30159cfeb7c2c12c5bcd9c37a4902af57824d7
David Chang <dhchang@gmail.com>
2020-01-15 14:27:57 -0500

    Simplify fish configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,11 +5,3 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
-
-if type -q keychain
-    eval (keychain -q --eval id_rsa)
-end
-
-if test -e ~/.profile
-    source ~/.profile
-end
