113f7efe55e3000f5e1274e33dc56fb3991e7a7c
David Chang <dhchang@gmail.com>
2020-01-23 22:38:06 -0500

    Make dwm battery status dynamic

--- dwm/.config/dwm/launch.sh
+++ dwm/.config/dwm/launch.sh
@@ -4,12 +4,16 @@ pwifi() {
   echo " $(iw dev | grep ssid | awk '{print $2}')"
 }
 
-pbat() {
-  echo " $(acpi | grep -Po '\d+%')"
-}
-
 pvol() {
   echo " $(amixer get Master | awk -F'[][]' 'END{print $2}')"
+}
+
+pbat() {
+  icons=('' '' '' '' '' '')
+  re=': (\w*), (.*)%,' 
+  [[ `acpi` =~ $re ]]
+  [[ ${BASH_REMATCH[1]} = 'Charging' ]] && icon='' || icon=${icons[BASH_REMATCH[2] / 20]}
+  echo "${icon} ${BASH_REMATCH[2]}%"
 }
 
 pdate() {
@@ -17,7 +21,7 @@ pdate() {
 }
 
 while :; do
-  xsetroot -name "$(pwifi)  $(pbat)  $(pvol)  $(pdate)"
+  xsetroot -name "$(pwifi)  $(pvol)  $(pbat)  $(pdate)"
   sleep 1
 done
 
