64e8a08105f11c3719b8e96bd4462083263accf0
David Chang <dhchang@gmail.com>
2020-02-08 11:24:40 -0500

    Change working dir for vim abbreviations

--- fish/.config/fish/conf.d/custom.fish
+++ fish/.config/fish/conf.d/custom.fish
@@ -5,6 +5,6 @@ abbr -a skg 'sk --ansi -ic \'rg --color=ansi --hidden -in "{}"\''
 # vim
 abbr -a vim nvim
 abbr -a v nvim
-abbr -a vw nvim -c VimwikiIndex
-abbr -a vd nvim -c VimwikiMakeDiaryNote
-abbr -a vrc nvim ~/.config/nvim/init.vim
+abbr -a vw 'nvim -c VimwikiIndex -c "cd %:p:h"'
+abbr -a vd 'nvim -c VimwikiMakeDiaryNote -c "cd %:p:h"'
+abbr -a vrc 'nvim ~/.config/nvim/init.vim -c "cd ~/.config"'
