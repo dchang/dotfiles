9f1d7c03f8fefaa8dc67fba1cb5d1d2fa8716988
David Chang <dhchang@gmail.com>
2020-02-08 13:01:19 -0500

    Remove fish plugins from git

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,17 @@ if status is-login
   end
 end
 
+# skim
+alias sk='sk --color=fg:8,hl:1,hl+:1'
+abbr -a skg 'sk --ansi -ic \'rg --color=ansi --hidden -in "{}"\''
+
+# vim
+abbr -a vim nvim
+abbr -a v nvim
+abbr -a vw 'nvim -c VimwikiIndex -c "cd %:p:h"'
+abbr -a vd 'nvim -c VimwikiMakeDiaryNote -c "cd %:p:h"'
+abbr -a vrc 'nvim ~/.config/nvim/init.vim -c "cd ~/.config"'
+
 if test -e ~/.profile
   . ~/.profile
 end
