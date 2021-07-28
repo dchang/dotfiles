f7231e1a5678bfcd62b83a660c5118bf866f261e
David Chang <dhchang@gmail.com>
2021-07-28 15:00:40 -0400

    Add lsp mappings and test out status lines

--- nvim-0.5/.config/nvim/init.lua
+++ nvim-0.5/.config/nvim/init.lua
@@ -5,6 +5,7 @@
 - https://oroques.dev/notes/neovim-init/
 - https://alpha2phi.medium.com/neovim-init-lua-e80f4f136030
 - https://github.com/Gavinok/dotneovim
+- https://sharksforarms.dev/posts/neovim-rust/
 --]]
 
 local cmd = vim.cmd
@@ -21,10 +22,15 @@ end
 require 'paq' {
 	'savq/paq-nvim';
 	'lifepillar/vim-gruvbox8';
-	'dstein64/vim-startuptime';
 	'junegunn/fzf';
 	'junegunn/fzf.vim';
 	'vimwiki/vimwiki';
+	'neovim/nvim-lspconfig';
+	--'dstein64/vim-startuptime';
+	--'nvim-lua/lsp-status.nvim';
+	'hoob3rt/lualine.nvim';
+	--'kyazdani42/nvim-web-devicons';
+	--'ryanoasis/vim-devicons';
 }
 
 cmd 'colorscheme gruvbox8_hard'
@@ -35,7 +41,7 @@ opt.shiftwidth = 0        -- use tabstop
 
 g.mapleader = ' '
 
-local function nnoremap(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, {noremap = true}) end
+local nnoremap = function(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, {noremap = true}) end
 
 nnoremap('Y', 'y$')
 nnoremap('<leader>fw',  ':Windows<CR>')
@@ -51,6 +57,38 @@ nnoremap('<leader>fm',  ':Maps<CR>')
 
 g.vimwiki_list = { { path = '~/Documents/wiki', syntax = 'markdown', ext = '.md' } }
 
+nnoremap('gD',  '<cmd>lua vim.lsp.buf.declaration()<CR>')
+nnoremap('gd',  '<cmd>lua vim.lsp.buf.definition()<CR>')
+nnoremap('K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+nnoremap('gi',  '<cmd>lua vim.lsp.buf.implementation()<CR>')
+nnoremap('<C-k>',  '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+nnoremap('<leader>wa',  '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
+nnoremap('<leader>wr',  '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+nnoremap('<leader>wl',  '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
+nnoremap('<leader>D',  '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+nnoremap('<leader>rn',  '<cmd>lua vim.lsp.buf.rename()<CR>')
+nnoremap('<leader>ca',  '<cmd>lua vim.lsp.buf.code_action()<CR>')
+nnoremap('gr',  '<cmd>lua vim.lsp.buf.references()<CR>')
+nnoremap('<leader>e',  '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+nnoremap('[d',  '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+nnoremap(']d',  '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+nnoremap('<leader>ds',  '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>')
+nnoremap('<leader>df',  '<cmd>lua vim.lsp.buf.formatting()<CR>')
+
+--local lsp_status = require('lsp-status')
+--lsp_status.register_progress()
+local lspconfig = require('lspconfig')
+lspconfig.rust_analyzer.setup{
+	--on_attach = lsp_status.on_attach,
+	--capabilities = lsp_status.capabilities
+}
+--local sl = ''
+--sl = sl..[[%!luaeval("require('lsp-status').status()")]]
+--opt.statusline = sl
+
+require('lualine').setup()
+
+
 --[[
 ## TODO
 
@@ -61,5 +99,6 @@ g.vimwiki_list = { { path = '~/Documents/wiki', syntax = 'markdown', ext = '.md'
 - [ ] Tree-sitter
 - [ ] LSP - Lua, Rust, JS
 - [ ] Vimwiki
+- [ ] lsp-status
 --]]
 
