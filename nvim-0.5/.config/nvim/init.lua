3c0c0cd7d64f760c84499bf7bbc052452f275e49
David Chang <dhchang@gmail.com>
2021-07-29 22:02:24 -0400

    Add nvim format on save and some tidying

--- nvim-0.5/.config/nvim/init.lua
+++ nvim-0.5/.config/nvim/init.lua
@@ -12,6 +12,7 @@ local cmd = vim.cmd
 local fn = vim.fn
 local g = vim.g
 local opt = vim.opt
+local api = vim.api
 
 local paq_path = fn.stdpath('data')..'/site/pack/paqs/opt/paq-nvim'
 if fn.empty(fn.glob(paq_path)) > 0 then
@@ -21,17 +22,18 @@ end
 
 require 'paq' {
 	'savq/paq-nvim';
+	'tweekmonster/startuptime.vim';
 	'lifepillar/vim-gruvbox8';
 	'junegunn/fzf';
 	'junegunn/fzf.vim';
 	'vimwiki/vimwiki';
-	'neovim/nvim-lspconfig';
-	--'dstein64/vim-startuptime';
-	'nvim-lua/lsp-status.nvim';
 	'nvim-lua/plenary.nvim';
+	'neovim/nvim-lspconfig';
+	'nvim-lua/lsp-status.nvim';
 	'hoob3rt/lualine.nvim';
 	--'kyazdani42/nvim-web-devicons';
 	--'ryanoasis/vim-devicons';
+	--'dstein64/vim-startuptime';
 }
 
 cmd 'colorscheme gruvbox8_hard'
@@ -41,11 +43,13 @@ opt.tabstop = 4           -- tab size
 opt.shiftwidth = 0        -- use tabstop
 
 g.mapleader = ' '
+g.vimwiki_list = { { path = '~/Documents/wiki', syntax = 'markdown', ext = '.md' } }
 
-local nnoremap = function(lhs, rhs) vim.api.nvim_set_keymap('n', lhs, rhs, {noremap = true}) end
+local nnoremap = function(lhs, rhs) api.nvim_set_keymap('n', lhs, rhs, {noremap = true}) end
 
 nnoremap('Y', 'y$')
 nnoremap('<leader>sv', ':source $MYVIMRC<CR>')
+
 nnoremap('<leader>fw',  ':Windows<CR>')
 nnoremap('<leader>fa',  ':Files<CR>')
 nnoremap('<leader>fs',  ':Rg<CR>')
@@ -57,33 +61,34 @@ nnoremap('<leader>fc',  ':Commits<CR>')
 nnoremap('<leader>fb',  ':BCommits<CR>')
 nnoremap('<leader>fm',  ':Maps<CR>')
 
-g.vimwiki_list = { { path = '~/Documents/wiki', syntax = 'markdown', ext = '.md' } }
-
-nnoremap('gD',  '<cmd>lua vim.lsp.buf.declaration()<CR>')
-nnoremap('gd',  '<cmd>lua vim.lsp.buf.definition()<CR>')
-nnoremap('K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
-nnoremap('gi',  '<cmd>lua vim.lsp.buf.implementation()<CR>')
-nnoremap('<C-k>',  '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-nnoremap('<leader>wa',  '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
-nnoremap('<leader>wr',  '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
-nnoremap('<leader>wl',  '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
-nnoremap('<leader>D',  '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-nnoremap('<leader>rn',  '<cmd>lua vim.lsp.buf.rename()<CR>')
-nnoremap('<leader>ca',  '<cmd>lua vim.lsp.buf.code_action()<CR>')
-nnoremap('gr',  '<cmd>lua vim.lsp.buf.references()<CR>')
-nnoremap('<leader>e',  '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
-nnoremap('[d',  '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-nnoremap(']d',  '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-nnoremap('<leader>ds',  '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>')
-nnoremap('<leader>df',  '<cmd>lua vim.lsp.buf.formatting()<CR>')
+nnoremap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+nnoremap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+nnoremap('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+nnoremap('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+nnoremap('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+nnoremap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+nnoremap('<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
+nnoremap('<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+nnoremap('<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
+nnoremap('<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+nnoremap('<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+nnoremap('<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+nnoremap('<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+nnoremap('[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+nnoremap(']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+nnoremap('<leader>ds', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>')
+nnoremap('<leader>df', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 
 local lsp_status = require('lsp-status')
 lsp_status.register_progress()
+
 local lspconfig = require('lspconfig')
 lspconfig.rust_analyzer.setup{
 	on_attach = lsp_status.on_attach,
 	capabilities = lsp_status.capabilities
 }
+
+api.nvim_exec('autocmd BufWritePre *.rs lua vim.lsp.buf.formatting()', false)
 
 require'plenary.reload'.reload_module('lualine', true)
 require'lualine'.setup {
