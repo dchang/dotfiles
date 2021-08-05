f7f1e9c81d289de6347bd5464ab32e11a3339249
David Chang <dhchang@gmail.com>
2021-08-04 20:09:54 -0400

    nvim-lsp: completion wip

--- nvim-lsp/.config/nvim/init.lua
+++ nvim-lsp/.config/nvim/init.lua
@@ -41,8 +41,10 @@ require 'paq' {
     'vimwiki/vimwiki';
     --'nvim-lua/plenary.nvim';
     'neovim/nvim-lspconfig';
-    'nvim-lua/lsp-status.nvim';
     'nvim-treesitter/nvim-treesitter';
+    --'nvim-lua/completion-nvim';
+    --'hrsh7th/nvim-compe';
+    'nvim-lua/lsp-status.nvim';
     'hoob3rt/lualine.nvim';
     --'kyazdani42/nvim-web-devicons';
     --'ryanoasis/vim-devicons';
@@ -97,12 +99,56 @@ nnoremap(']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
 nnoremap('<leader>ds', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>')
 nnoremap('<leader>df', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 
-local lsp_status = require('lsp-status')
-lsp_status.register_progress()
+--[[
+vim.o.completeopt = 'menuone,noselect'
+require'compe'.setup{
+    enabled = true;
+    autocomplete = true;
+    debug = false;
+    min_length = 1;
+    preselect = 'enable';
+    throttle_time = 80;
+    source_timeout = 200;
+    resolve_timeout = 800;
+    incomplete_delay = 400;
+    max_abbr_width = 100;
+    max_kind_width = 100;
+    max_menu_width = 100;
+    documentation = {
+        border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+        winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+        max_width = 120,
+        min_width = 60,
+        max_height = math.floor(vim.o.lines * 0.3),
+        min_height = 1,
+    };
+    source = {
+        path = true;
+        buffer = true;
+        calc = true;
+        nvim_lsp = true;
+        nvim_lua = true;
+        vsnip = false;
+        ultisnips = true;
+        luasnip = true;
+    };
+}
+local inoremap = function(lhs, rhs) api.nvim_set_keymap('i', lhs, rhs, {expr = true}) end
+inoremap('<C-s>', 'compe#complete()')
+inoremap('<CR>', 'compe#confirm("<CR>")')
+inoremap('<C-e>', 'compe#close("<C-e>")')
+inoremap('<C-f>', 'compe#scroll({ "delta": +4 })')
+inoremap('<C-d>', 'compe#scroll({ "delta": -4 })')
+--]]
 
 local lspconfig = require('lspconfig')
+local lsp_status = require('lsp-status')
+lsp_status.register_progress()
 lspconfig.rust_analyzer.setup{
-    on_attach = lsp_status.on_attach,
+    on_attach = function(client, bufnr)
+        --require'completion'.on_attach(client, bufnr)
+        lsp_status.on_attach(client, bufnr)
+    end,
     capabilities = lsp_status.capabilities
 }
 
