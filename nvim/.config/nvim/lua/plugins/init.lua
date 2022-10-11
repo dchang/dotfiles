bd56c8c02c69ddfb7cf34d69657fd374b43550e5
David Chang <dhchang@gmail.com>
2022-10-10 20:04:25 -0400

    Add nvim completion

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -16,14 +16,24 @@ require 'paq' {
     'neovim/nvim-lspconfig';
     'nvim-treesitter/nvim-treesitter';
 
+    'hrsh7th/cmp-nvim-lsp';
+    'hrsh7th/cmp-buffer';
+    'hrsh7th/cmp-path';
+    'hrsh7th/cmp-cmdline';
+    'hrsh7th/nvim-cmp';
+
+    'L3MON4D3/LuaSnip';
+    'saadparwaiz1/cmp_luasnip';
+
     --'nvim-lua/lsp-status.nvim';
     --'feline-nvim/feline.nvim';
     'nvim-lualine/lualine.nvim';
     'kyazdani42/nvim-web-devicons';
 
-    'vimwiki/vimwiki';
+    { 'vimwiki/vimwiki' };
 
     --'tweekmonster/startuptime.vim';
+    --'dstein64/vim-startuptime';
 
     --'lifepillar/vim-gruvbox8';
     --'folke/tokyonight.nvim';
