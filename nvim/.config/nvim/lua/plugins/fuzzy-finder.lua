2911486d47e74f6ce3fcb1c3f9de2c5c0d870d99
David Chang <dhchang@gmail.com>
2022-11-04 12:29:29 -0400

    nvim: allow hidden files in telescope file_files

--- nvim/.config/nvim/lua/plugins/fuzzy-finder.lua
+++ nvim/.config/nvim/lua/plugins/fuzzy-finder.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', function()
+    builtin.find_files({ find_command = { 'rg', '--files', '--hidden', '-g', '!.git' } })
+end, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
