58b18d5e4c030588d99d3a5904e0b0fe2a543951
David Chang <dhchang@gmail.com>
2022-11-14 21:55:17 -0500

    nvim: some buffer keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,7 +2,12 @@ vim.g.mapleader = ' '
 
 vim.keymap.set({ 'n', 'v' }, '<leader><space>', ':')
 
+vim.keymap.set('n', '<leader>l', ':bn<CR>') -- next buffer
+vim.keymap.set('n', '<leader>h', ':bp<CR>') -- previous buffer
+vim.keymap.set('n', '<leader>bd', ':bd<CR>') -- unload buffer
+vim.keymap.set('n', '<leader>ba', ':%bd<CR>') -- unload all buffers
+
+vim.keymap.set('n', '<leader>/', ':noh<CR>', { silent = true }) -- stop highlighting search matches
+
 vim.keymap.set('n', '<leader>rl', ':luafile %<CR>') -- execute lua script in current buffer
 vim.keymap.set('n', '<leader>rr', ':source $MYVIMRC<CR>') -- reload config
-vim.keymap.set('n', '<leader>ba', ':%bd<CR>') -- unload all buffers
-vim.keymap.set('n', '<leader>/', ':noh<CR>', { silent = true }) -- stop highlighting search matches
