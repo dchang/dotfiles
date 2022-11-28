f68bcd1a9dcd13de6c1c613f2dd035917a58ebd8
David Chang <dhchang@gmail.com>
2022-11-28 16:09:35 -0500

    tmux: change default terminal

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -8,6 +8,5 @@ opt.tabstop = 4 -- tab size
 opt.shiftwidth = 0 -- use tabstop
 opt.swapfile = false -- disable swapfile creation
 opt.hidden = true -- allow unsaved buffer change
-opt.termguicolors = true -- enable 24-bit color
 opt.scrolloff = 8 -- minimal number of screen lines to keep above and below the cursor
 opt.signcolumn = 'yes' -- always draw the signcolumn
