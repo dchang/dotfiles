8e6375d2519c75e9bb691a5a3bb162c18f0303f0
David Chang <dhchang@gmail.com>
2022-12-02 10:30:28 -0500

    nvim: add close other buffers keymap

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -4,8 +4,9 @@ vim.keymap.set({ 'n', 'v' }, '<leader><space>', ':')
 
 vim.keymap.set('n', '<leader>l', ':bn<CR>') -- next buffer
 vim.keymap.set('n', '<leader>h', ':bp<CR>') -- previous buffer
-vim.keymap.set('n', '<leader>bd', ':bd<CR>') -- unload buffer
+vim.keymap.set('n', '<leader>bd', ':bd<CR>') -- unload current buffer
 vim.keymap.set('n', '<leader>ba', ':%bd<CR>') -- unload all buffers
+vim.keymap.set('n', '<leader>bo', ':%bd|e#<CR>') -- unload all buffers except current
 
 vim.keymap.set('n', '<leader>/', ':noh<CR>', { silent = true }) -- stop highlighting search matches
 
