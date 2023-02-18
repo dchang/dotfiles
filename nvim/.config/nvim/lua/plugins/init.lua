1827bc21be99daae2176299bd193c34b86c53cee
David Chang <dhchang@gmail.com>
2023-02-18 15:55:14 -0500

    nvim: Change plugin manager to Lazy

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,86 +1,72 @@
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.0',
-        requires = { 'nvim-lua/plenary.nvim' }
-    }
-    use 'neovim/nvim-lspconfig'
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update { with_sync = true } end
-    }
-    use {
-        'hrsh7th/nvim-cmp',
-        requires = {
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-        }
-    }
-    use 'kyazdani42/nvim-web-devicons'
-    use 'elihunter173/dirbuf.nvim'
-    use {
-        'nvim-lualine/lualine.nvim',
-        config = function()
-            require('lualine').setup {
-                sections = {
-                    lualine_c = {
-                        {
-                            'filename',
-                            path = 1,
-                        }
-                    }
-                }
-            }
-        end
-    }
-    use { 'rebelot/kanagawa.nvim', config = 'vim.cmd[[colorscheme kanagawa]]' }
-    -- use { 'folke/tokyonight.nvim', config = 'vim.cmd[[colorscheme tokyonight]]' }
-    use { 'lewis6991/gitsigns.nvim', config = function() require('gitsigns').setup() end }
-    use { 'j-hui/fidget.nvim', config = function() require('fidget').setup() end }
-    --[[
-    use {
-        'folke/noice.nvim',
-        config = function()
-            require('noice').setup()
-            -- require('telescope').load_extension('noice')
-        end,
-        requires = {
-            'MunifTanjim/nui.nvim',
-            -- 'rcarriga/nvim-notify',
-        }
-    }
-    --]]
-
-    --[[
-    use {
-        'vimwiki/vimwiki',
-        config = function()
-            vim.g.vimwiki_list = { { path = '~/Documents/wiki/', syntax = 'markdown', ext = '.md' } }
-            vim.g.vimwiki_global_ext = 0
-        end
-    }
-    --]]
-    use { 'renerocksai/telekasten.nvim' }
-
-    if packer_bootstrap then
-        require('packer').sync()
+require("lazy").setup({
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      vim.cmd("colorscheme kanagawa")
     end
-end)
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  "neovim/nvim-lspconfig",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require('nvim-treesitter.install').update { with_sync = true }
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    }
+  },
+  "kyazdani42/nvim-web-devicons",
+  "elihunter173/dirbuf.nvim",
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require('lualine').setup {
+        sections = {
+          lualine_c = {
+            { 'filename', path = 1 }
+          }
+        }
+      }
+    end
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+  {
+    "j-hui/fidget.nvim",
+    config = function()
+      require('fidget').setup()
+    end
+  },
+  "renerocksai/telekasten.nvim",
+})
+
