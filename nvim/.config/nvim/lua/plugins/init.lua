59d24060ba920facb15994854c1806293f661d60
David Chang <dhchang@gmail.com>
2023-02-18 16:42:16 -0500

    nvim: Testing neorg

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -67,5 +67,24 @@ require("lazy").setup({
             require('fidget').setup()
         end
     },
-    "renerocksai/telekasten.nvim",
+    --"renerocksai/telekasten.nvim",
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        opts = {
+            load = {
+                ["core.defaults"] = {}, -- Loads default behaviour
+                ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.norg.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/Documents/neorg",
+                        },
+                        default_workspace = "notes",
+                    },
+                },
+            },
+        },
+        dependencies = { { "nvim-lua/plenary.nvim" } },
+    },
 })
