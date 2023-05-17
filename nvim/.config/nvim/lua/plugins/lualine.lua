cbeba39b9727082803e6b5362adfae1b8109b7a5
David Chang <dhchang@gmail.com>
2023-05-17 09:39:04 -0400

    nvim: default lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,13 +2,7 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         config = function()
-            require('lualine').setup {
-                sections = {
-                    lualine_c = {
-                        { 'filename', path = 1 }
-                    }
-                }
-            }
+            require('lualine').setup {}
         end
     }
 }
