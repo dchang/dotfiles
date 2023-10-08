c23826c4af72560232f1206d536eb8e62289d433
David Chang <dhchang@gmail.com>
2023-10-08 14:55:42 -0400

    nvim: fuzzy find diary dir, remove kiwi keymaps except new diary entry

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -17,18 +17,19 @@ return {
             vim.keymap.set('n', '<leader>fc', builtin.commands, {})
             vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
 
+            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
             vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
             vim.keymap.set('n', '<leader>fi', builtin.lsp_incoming_calls, {})
             vim.keymap.set('n', '<leader>fo', builtin.lsp_outgoing_calls, {})
             vim.keymap.set('n', '<leader>fl', function()
-                builtin.lsp_document_symbols {
-                    symbols = { "enum", "function", "struct" }
-                }
+                builtin.lsp_document_symbols({ symbols = { "enum", "function", "struct" } })
             end, {})
-            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
 
             vim.keymap.set('n', '<leader>fw', function()
                 builtin.find_files({ cwd = "~/Documents/wiki" })
+            end, {})
+            vim.keymap.set('n', '<leader>fj', function()
+                builtin.find_files({ cwd = "~/Documents/wiki/diary" })
             end, {})
         end
     }
