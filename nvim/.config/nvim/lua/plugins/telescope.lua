78d639cdca470cb763a41823bc3b1a0b6c8315ee
David Chang <dhchang@gmail.com>
2023-10-11 09:27:32 -0400

    nvim: clean up telescope config

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,36 +1,39 @@
+-- https://github.com/nvim-telescope/telescope.nvim
 return {
     {
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
-            local builtin = require('telescope.builtin')
+            local lib = require("telescope.builtin")
 
-            vim.keymap.set('n', '<leader>ff', function()
-                builtin.find_files({ find_command = { 'rg', '--files', '--hidden', '-g', '!.git' } })
-            end, {})
-            vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            -- file pickers
+            vim.keymap.set("n", "<leader>ff", function()
+                lib.find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
+            end)
+            vim.keymap.set("n", "<leader>fg", lib.git_files)
+            vim.keymap.set("n", "<leader>fs", lib.live_grep)
+            vim.keymap.set("n", "<leader>fw", function()
+                lib.find_files({ cwd = "~/Documents/wiki" })
+            end)
+            vim.keymap.set("n", "<leader>fj", function()
+                lib.find_files({ cwd = "~/Documents/wiki/diary" })
+            end)
 
-            vim.keymap.set('n', '<leader>fw', builtin.oldfiles, {})
-            vim.keymap.set('n', '<leader>fc', builtin.commands, {})
-            vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+            -- vim pickers
+            vim.keymap.set("n", "<leader>fb", lib.buffers)
+            vim.keymap.set("n", "<leader>fv", lib.oldfiles)
+            vim.keymap.set("n", "<leader>fc", lib.commands)
+            vim.keymap.set("n", "<leader>fh", lib.help_tags)
+            vim.keymap.set("n", "<leader>fk", lib.keymaps)
 
-            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-            vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
-            vim.keymap.set('n', '<leader>fi', builtin.lsp_incoming_calls, {})
-            vim.keymap.set('n', '<leader>fo', builtin.lsp_outgoing_calls, {})
-            vim.keymap.set('n', '<leader>fl', function()
-                builtin.lsp_document_symbols({ symbols = { "enum", "function", "struct" } })
-            end, {})
-
-            vim.keymap.set('n', '<leader>fw', function()
-                builtin.find_files({ cwd = "~/Documents/wiki" })
-            end, {})
-            vim.keymap.set('n', '<leader>fj', function()
-                builtin.find_files({ cwd = "~/Documents/wiki/diary" })
-            end, {})
+            -- lsp pickers
+            vim.keymap.set("n", "<leader>fr", lib.lsp_references)
+            vim.keymap.set("n", "<leader>fi", lib.lsp_incoming_calls)
+            vim.keymap.set("n", "<leader>fo", lib.lsp_outgoing_calls)
+            vim.keymap.set("n", "<leader>fl", function()
+                lib.lsp_document_symbols({ symbols = { "enum", "function", "struct" } })
+            end)
+            vim.keymap.set("n", "<leader>fd", lib.diagnostics)
         end
     }
 }
