81d7dd6c194e0fb13561f3dbbebd188ce1d52953
David Chang <dhchang@gmail.com>
2023-10-11 22:19:01 -0400

    nvim: tidy up telescope config

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -13,35 +13,32 @@ return {
         config = function(_, opts)
             require("telescope").setup(opts)
             local lib = require("telescope.builtin")
+            local vks = vim.keymap.set
+            local cmd = function(fn, ...)
+                local args = { ... }
+                return function() fn(unpack(args)) end
+            end
 
             -- file pickers
-            vim.keymap.set("n", "<leader>ff", function()
-                lib.find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
-            end)
-            vim.keymap.set("n", "<leader>fg", lib.git_files)
-            vim.keymap.set("n", "<leader>fs", lib.live_grep)
-            vim.keymap.set("n", "<leader>fw", function()
-                lib.find_files({ cwd = "~/Documents/wiki" })
-            end)
-            vim.keymap.set("n", "<leader>fq", function()
-                lib.live_grep({ cwd = "~/Documents/wiki" })
-            end)
+            vks("n", "<leader>ff", cmd(lib.find_files, { find_command = { "rg", "--files", "--hidden", "-g", "!.git" } }))
+            vks("n", "<leader>fg", lib.git_files)
+            vks("n", "<leader>fs", lib.live_grep)
+            vks("n", "<leader>fw", cmd(lib.find_files, { cwd = "~/Documents/wiki" }))
+            vks("n", "<leader>fq", cmd(lib.live_grep, { cwd = "~/Documents/wiki" }))
 
             -- vim pickers
-            vim.keymap.set("n", "<leader>fb", lib.buffers)
-            vim.keymap.set("n", "<leader>fv", lib.oldfiles)
-            vim.keymap.set("n", "<leader>fc", lib.commands)
-            vim.keymap.set("n", "<leader>fh", lib.help_tags)
-            vim.keymap.set("n", "<leader>fk", lib.keymaps)
+            vks("n", "<leader>fb", lib.buffers)
+            vks("n", "<leader>fv", lib.oldfiles)
+            vks("n", "<leader>fc", lib.commands)
+            vks("n", "<leader>fh", lib.help_tags)
+            vks("n", "<leader>fk", lib.keymaps)
 
             -- lsp pickers
-            vim.keymap.set("n", "<leader>fr", lib.lsp_references)
-            vim.keymap.set("n", "<leader>fi", lib.lsp_incoming_calls)
-            vim.keymap.set("n", "<leader>fo", lib.lsp_outgoing_calls)
-            vim.keymap.set("n", "<leader>fl", function()
-                lib.lsp_document_symbols({ symbols = { "enum", "function", "struct" } })
-            end)
-            vim.keymap.set("n", "<leader>fd", lib.diagnostics)
+            vks("n", "<leader>fr", lib.lsp_references)
+            vks("n", "<leader>fi", lib.lsp_incoming_calls)
+            vks("n", "<leader>fo", lib.lsp_outgoing_calls)
+            vks("n", "<leader>fl", cmd(lib.lsp_document_symbols, { symbols = { "enum", "function", "struct" } }))
+            vks("n", "<leader>fd", lib.diagnostics)
         end
     }
 }
