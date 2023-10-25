de335041c43a9953c20966745cd5c042f721759f
David Chang <dhchang@gmail.com>
2023-10-24 21:05:53 -0400

    nvim: tidy up autocmds

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,8 +1,7 @@
-vim.api.nvim_create_augroup('default', { clear = true })
+local g = vim.api.nvim_create_augroup("Default", {})
 
 -- highlight yanks
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group = 'default',
-    pattern = '*',
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = g,
     callback = function() vim.highlight.on_yank() end
 })
