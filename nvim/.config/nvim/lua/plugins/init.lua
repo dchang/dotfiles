11bb8c5dd86e2e5acf32110d0762fbeaa683c230
David Chang <dhchang@gmail.com>
2023-10-29 16:33:46 -0400

    nvim: add markdown-preview.nvim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -35,6 +35,18 @@ return {
         build = ":MasonUpdate"
     },
     {
+        "iamcco/markdown-preview.nvim",
+        --cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+        config = function()
+            vim.keymap.set("n", "<leader>rm", "<cmd>:MarkdownPreview<cr>", { desc = "Preview markdown" })
+        end
+    },
+    {
         "stevearc/oil.nvim",
         enabled = false,
         config = function()
