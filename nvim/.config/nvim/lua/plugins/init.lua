1688d037e6d4f258d0ecdb4dd6b7139dee7667eb
David Chang <dhchang@gmail.com>
2023-10-29 11:22:12 -0400

    nvim: vetting file tree plugins

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -7,28 +7,9 @@ return {
         end,
     },
     {
-        "kyazdani42/nvim-web-devicons",
-        --"nvim-tree/nvim-web-devicons",
+        --"kyazdani42/nvim-web-devicons",
+        "nvim-tree/nvim-web-devicons",
         enabled = false,
-    },
-    {
-        "stevearc/oil.nvim",
-        enabled = false,
-        config = function()
-            require("oil").setup()
-            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-        end
-    },
-    {
-        "echasnovski/mini.nvim",
-        enabled = false,
-        version = "*",
-        config = function()
-            require("mini.files").setup()
-            vim.keymap.set("n", "-", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), false) end,
-                { desc = "Open parent directory" })
-
-        end
     },
     {
         "lewis6991/gitsigns.nvim",
@@ -48,5 +29,51 @@ return {
             require("mason").setup()
         end,
         build = ":MasonUpdate"
+    },
+    {
+        "stevearc/oil.nvim",
+        enabled = false,
+        config = function()
+            require("oil").setup()
+            vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+        end
+    },
+    {
+        "echasnovski/mini.nvim",
+        enabled = false,
+        version = "*",
+        config = function()
+            require("mini.files").setup()
+            vim.keymap.set("n", "-", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), false) end,
+                { desc = "Open parent directory" })
+
+        end
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        enabled = false,
+        version = "*",
+        lazy = false,
+        --dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("nvim-tree").setup()
+            --vim.keymap.set("n", "<leader>e", "<cmd>:NvimTreeToggle<cr>")
+            vim.keymap.set("n", "<leader>e", function() require("nvim-tree.api").tree.toggle() end)
+        end,
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        enabled = false,
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        config = function()
+            require("neo-tree").setup()
+            vim.keymap.set("n", "<leader>e", "<cmd>:Neotree toggle<cr>")
+        end
     },
 }
