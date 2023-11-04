eae020ba513e8f4fe3499293219367c0606207c6
David Chang <dhchang@gmail.com>
2023-11-04 12:21:36 -0400

    nvim: change lsp code related keymaps, remove workspace keymaps

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,49 +2,37 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local lspconfig = require('lspconfig')
+            local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
             local opts = { noremap = true, silent = true }
-            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+
+            vim.api.nvim_create_autocmd("BufWritePre", { callback = function() vim.lsp.buf.format() end })
+
             vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
             vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
             local on_attach = function(_, bufnr)
+                local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
                 vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-                local bufopts = { noremap = true, silent = true, buffer = bufnr }
                 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
                 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-                vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-                vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-                vim.keymap.set('n', '<leader>wl', function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, bufopts)
-                vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
                 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-                vim.keymap.set('n', '<A-F>', function() vim.lsp.buf.format { async = true } end, bufopts)
+                vim.keymap.set('n', '<leader>cs', vim.lsp.buf.signature_help, bufopts)
+                vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, bufopts)
+                vim.keymap.set('n', '<leader>cf', function() vim.lsp.buf.format { async = true } end, bufopts)
             end
 
-            vim.api.nvim_create_autocmd("BufWritePre", { callback = function() vim.lsp.buf.format() end })
-
-            local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-            local lspconfig = require('lspconfig')
-
-            lspconfig.denols.setup {
-                on_attach = on_attach,
-                capabilities = capabilities,
-                filetypes = {
-                    'markdown',
-                },
-            }
-
+            -- rust
             lspconfig.rust_analyzer.setup {
                 on_attach = on_attach,
                 capabilities = capabilities,
             }
 
+            -- lua
             local runtime_path = vim.split(package.path, ";")
             table.insert(runtime_path, "lua/?.lua")
             table.insert(runtime_path, "lua/?/init.lua")
@@ -70,14 +58,25 @@ return {
                 },
             }
 
+            -- sql
             lspconfig.sqlls.setup {
                 on_attach = on_attach,
                 capabilities = capabilities,
             }
 
+            -- typescript
             lspconfig.tsserver.setup {
                 on_attach = on_attach,
                 capabilities = capabilities,
+            }
+
+            -- javascript
+            lspconfig.denols.setup {
+                on_attach = on_attach,
+                capabilities = capabilities,
+                filetypes = {
+                    'markdown',
+                },
             }
         end
     }
