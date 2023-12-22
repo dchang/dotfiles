57098b073f3604f71416de3fb57095120c8654f3
David Chang <dhchang@gmail.com>
2023-12-22 14:43:30 -0500

    nvim: add dap and rust-tools for debugging

---
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        -- enabled = false,
+        dependencies = {
+            {
+                "rcarriga/nvim-dap-ui",
+                opts = {},
+                config = function(_, opts)
+                    local dap = require("dap")
+                    local dapui = require("dapui")
+                    dapui.setup(opts)
+                    dap.listeners.after.event_initialized["dapui_config"] = function()
+                        dapui.open({})
+                    end
+                    dap.listeners.before.event_terminated["dapui_config"] = function()
+                        dapui.close({})
+                    end
+                    dap.listeners.before.event_exited["dapui_config"] = function()
+                        dapui.close({})
+                    end
+                end,
+            },
+            {
+
+                "jay-babu/mason-nvim-dap.nvim",
+                dependencies = "mason.nvim",
+                cmd = { "DapInstall", "DapUninstall" },
+                opts = {
+                    automatic_installation = true,
+                    handlers = {},
+                    ensure_installed = {
+                        "codelldb",
+                    },
+                },
+
+            },
+        },
+    },
+}
