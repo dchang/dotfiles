caf1d3aced8d194d41f270027f3583e9c8679da1
David Chang <dhchang@gmail.com>
2024-07-31 13:39:11 -0400

    lazyvim: disable lsp inlay hints

--- LazyVim/.config/LazyVim/lua/plugins/override.lua
+++ LazyVim/.config/LazyVim/lua/plugins/override.lua
@@ -1,9 +1,18 @@
 return {
-  "lukas-reineke/headlines.nvim",
-  opts = {
-    markdown = {
-      fat_headline_upper_string = "▁",
-      fat_headline_lower_string = "▔",
+  {
+    "lukas-reineke/headlines.nvim",
+    opts = {
+      markdown = {
+        fat_headline_upper_string = "▁",
+        fat_headline_lower_string = "▔",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
     },
   },
 }
