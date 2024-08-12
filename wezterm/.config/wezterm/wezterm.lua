fc37c084f78b978f0dd56d2c611dc29203149bb6
David Chang <dhchang@gmail.com>
2024-08-12 10:35:18 -0400

    wezterm: change color scheme, format file

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'kanagawabones'
-config.default_domain = 'WSL:Ubuntu-22.04'
-config.font = wezterm.font 'JetBrainsMonoNL Nerd Font Mono'
+config.color_scheme = "Tokyo Night Moon"
+config.default_domain = "WSL:Ubuntu-22.04"
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
 config.font_size = 9.5
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
