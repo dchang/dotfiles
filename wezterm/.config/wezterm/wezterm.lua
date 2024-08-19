a2ea35ec55febc123c49cec5de114adab7c47870
David Chang <dhchang@gmail.com>
2024-08-19 15:19:22 -0400

    wezterm: disable ligatures

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,6 +11,7 @@ config.audible_bell = "Disabled"
 config.color_scheme = "Tokyo Night Moon"
 config.default_domain = "WSL:Ubuntu-22.04"
 config.font_size = 9.5
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.disable_default_key_bindings = true
