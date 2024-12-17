ee29e6bc7e9f3f344c0b394f8489d469dfdce9ac
David Chang <dhchang@gmail.com>
2024-12-17 10:32:13 -0500

    nvim: remove headlines plugin

--- nvim/.config/nvim/lua/plugins/override.lua
+++ nvim/.config/nvim/lua/plugins/override.lua
@@ -1,15 +1,5 @@
 return {
 	{
-		"lukas-reineke/headlines.nvim",
-		opts = {
-			markdown = {
-				fat_headline_upper_string = "▁",
-				fat_headline_lower_string = "▔",
-			},
-		},
-	},
-
-	{
 		"neovim/nvim-lspconfig",
 		opts = {
 			inlay_hints = { enabled = false },
