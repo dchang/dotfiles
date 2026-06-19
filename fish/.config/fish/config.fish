8547cfc04b37270254a53a12048296d221db8e69
David Chang <dhchang@gmail.com>
2026-06-19 13:02:57 -0400

    fish: add cargo test --test abbreviation, some refactoring

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,23 +12,30 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $BUN_INSTALL/bin
 
-if type -q $HOME/bin/homebrew/bin/brew
-    eval ($HOME/bin/homebrew/bin/brew shellenv)
-end
+# abbreviations
 
 abbr -a f 'find . | rg'
 abbr -a h 'history | rg'
 abbr -a mkdir 'mkdir -pv'
+abbr -a tldrf 'tldr -l | fzf --preview "tldr {1} --color=always" --preview-window=right,80% | xargs tldr'
+
 abbr -a v nvim
 abbr -a vw 'nvim -c "cd ~/Documents/wiki/"'
 abbr -a vcf 'nvim ~/.config/fish/config.fish -c "cd %:p:h"'
+
 abbr -a c cargo
 abbr -a ccl 'cargo clippy'
 abbr -a ce 'RUST_LOG=debug cargo r --example'
 abbr -a cr 'RUST_LOG=debug cargo r --'
 abbr -a ct 'cargo t --'
+abbr -a ctt 'cargo t --tests'
 abbr -a cu 'cargo update'
-abbr -a tldrf 'tldr -l | fzf --preview "tldr {1} --color=always" --preview-window=right,80% | xargs tldr'
+
+# platform-specific
+
+if type -q $HOME/bin/homebrew/bin/brew
+    eval ($HOME/bin/homebrew/bin/brew shellenv)
+end
 
 if type -q keychain
     eval (keychain -q --eval id_ed25519)
